d57cf9dc35f85382dbcf3c5375304a43f9c62c6a
Jo <joridjonland@gmail.com>
2019-05-19 13:02:31 +0100

    express skeleton

---
+++ textgen/name_compare_new.py
@@ -0,0 +1,85 @@
+import os, sys
+
+genfile = sys.argv[1]
+checkfile = sys.argv[2]
+
+checklist = []
+genlist = []
+
+subitemlist = []
+
+isbirdlist = []
+partbirdlist = []
+
+with open(checkfile, 'r') as file:
+	for line in file:
+		current = line[:-1]
+		checklist.append(current)
+
+with open(genfile, 'r') as file:
+	for line in file:
+		current = line[:-1]
+		genlist.append(current)
+
+print('Number of generated names:', len(genlist))
+
+i = 0
+listlength = len(genlist)
+
+for it in range(listlength):	
+	if genlist[i] in checklist:
+		#print(genlist[i], 'is in checklist')
+		isbirdlist.append(genlist[i])
+		genlist.pop(i)
+	else:
+		i = i + 1
+	
+print('Number of duplicate names:', len(isbirdlist))
+print('Number of unique names:', len(genlist))
+
+for item in checklist:
+	newitem = item.split()
+	for subitem in newitem:
+		subitemlist.append(subitem)
+
+i = 0
+j = 0
+listlength = len(genlist)
+
+for it in range(listlength):
+	genitemlist = genlist[i].split()
+	j = 0
+	for jit in range(len(genitemlist)):
+		#print(genitemlist[j] in checklist)
+		if genitemlist[j] in subitemlist:
+			partbirdlist.append(genlist[i])
+			genlist.pop(i)
+			i = i - 1
+			break
+		j = j + 1
+	i = i + 1
+
+print('Number of part duplicate names:', len(partbirdlist))
+print('Number of wholly unique names:', len(genlist))
+
+print('-----')
+print('Wholly unique names:')
+for item in genlist:
+	print(item)
+
+f, e = os.path.splitext(genfile)
+f = f + "_duplicates" + e
+textfile = open(f, "w+")
+
+textfile.write("Duplicate names: \r\n")
+for item in isbirdlist:
+	textfile.write("{!s} \r\n".format(item))
+
+textfile.write("\r\nPart duplicate names:\r\n")
+for item in partbirdlist:
+	textfile.write("{!s} \r\n".format(item))
+
+textfile.write("\r\nUnique names:\r\n")
+for item in genlist:
+	textfile.write("{!s} \r\n".format(item))
+
---
+++ textgen/textgen.py
@@ -0,0 +1,49 @@
+from textgenrnn import textgenrnn
+
+model_cfg = {
+    'word_level': False,   # set to True if want to train a word-level model (requires more data and smaller max_length)
+    'rnn_size': 128,   # number of LSTM cells of each layer (128/256 recommended)
+    'rnn_layers': 2,   # number of LSTM layers (>=2 recommended)
+    'rnn_bidirectional': False,   # consider text both forwards and backward, can give a training boost
+    'max_length': 30,   # number of tokens to consider before predicting the next (20-40 for characters, 5-10 for words recommended)
+    'max_words': 10000,   # maximum number of words to model; the rest will be ignored (word-level model only)
+}
+
+train_cfg = {
+    'line_delimited': True,   # set to True if each text has its own line in the source file
+    'num_epochs': 12,   # set higher to train the model for longer
+    'gen_epochs': 4,   # generates sample text from model after given number of epochs
+    'train_size': 0.7,   # proportion of input data to train on: setting < 1.0 limits model from learning perfectly
+    'dropout': 0.0,   # ignore a random proportion of source tokens each epoch, allowing model to generalize better
+    'validation': True,   # If train__size < 1.0, test on holdout dataset; will make overall training slower
+    'is_csv': False   # set to True if file is a CSV exported from Excel/BigQuery/pandas
+}
+
+file_name = "birdnames.txt"
+model_name = 'bird_model'   # change to set file name of resulting trained models/texts
+
+textgen = textgenrnn(name=model_name)
+
+train_function = textgen.train_from_file if train_cfg['line_delimited'] else textgen.train_from_largetext_file
+
+train_function(
+    file_path=file_name,
+    new_model=True,
+    num_epochs=train_cfg['num_epochs'],
+    gen_epochs=train_cfg['gen_epochs'],
+    batch_size=1024,
+    train_size=train_cfg['train_size'],
+    dropout=train_cfg['dropout'],
+    validation=train_cfg['validation'],
+    is_csv=train_cfg['is_csv'],
+    rnn_layers=model_cfg['rnn_layers'],
+    rnn_size=model_cfg['rnn_size'],
+    rnn_bidirectional=model_cfg['rnn_bidirectional'],
+    max_length=model_cfg['max_length'],
+    dim_embeddings=100,
+    word_level=model_cfg['word_level'])
+
+gen_file = 'generated_birdnames.txt'
+textgen.generate_to_file(gen_file, temperature=1.0, prefix=None, n = 500, max_gen_length=300)
+
+
